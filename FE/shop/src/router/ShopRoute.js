ff2be57795cb1943eaebd0d02e43720f267b7535
lambaoduy <22130057@st.hcmuaf.edu.vn>
2026-02-25 23:26:31 +0700

    Wishlist

--- FE/shop/src/router/ShopRoute.js
+++ FE/shop/src/router/ShopRoute.js
@@ -3,6 +3,7 @@ import Home from '@/views/shop/Home.vue'
 import ProductDetail from '@/views/shop/ProductDetail.vue'
 import Cart from '@/views/shop/Cart.vue'
 import SignUP from '@/views/shop/SignUP.vue'
+import WishList from '@/views/shop/WishList.vue'
 
 export default [
   {
@@ -12,7 +13,8 @@ export default [
       { path: '', name: 'home', component: Home },
       { path: 'product/:id', name: 'product-detail', component: ProductDetail },
       { path: 'cart', name: 'cart', component: Cart },
-      { path: 'signup', name: 'signup', component: SignUP},
+      { path: 'signup', name: 'signup', component: SignUP },
+      { path: 'wishlist', name: 'wishlist', component: WishList },
     ],
   },
 ]
